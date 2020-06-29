af08f52cfb28edd41d962ec354d70b7860f45049
KID <50666207+KID421@users.noreply.github.com>
2020-06-29 18:56:43 +0800

    攻擊效果

--- CardGame/Assets/Scripts/AttackCard.cs
+++ CardGame/Assets/Scripts/AttackCard.cs
@@ -1,21 +1,29 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI.Extensions;        // 引用 UI 額外功能 API
+using System.Collections;
 
 // 要求元件(UI 線條渲染)
-public class AttackCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class AttackCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerEnterHandler
 {
     private UILineRenderer line;     // UI 線條渲染
     private Transform arrow;
     private GameObject lineObj;
-    private Vector2 original;
+    
+    private Vector2 posStart;
+    private Vector2 posEnd;
+
+    private static bool attack;
+    private static Transform parent;
+    private static Transform cardAttack;
+    private Transform cardTarget;
 
     private void Awake()
     {
         arrow = GameObject.Find("箭頭").transform;
         lineObj = GameObject.Find("線條");
-        line = lineObj.GetComponent<UILineRenderer>();                  // 取得
-        line.material = Resources.Load<Material>("線條材質");    // 設定材質
+        line = lineObj.GetComponent<UILineRenderer>();
+        line.material = Resources.Load<Material>("線條材質");
         line.sprite = Resources.Load<Sprite>("線條");
         line.Points = new Vector2[] { Vector2.zero, Vector2.zero };
         line.LineThickness = 100;
@@ -24,31 +32,57 @@ public class AttackCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        RectTransform rect = GetComponent<RectTransform>();
-        original = transform.position;
-        original.x = transform.position.x + rect.sizeDelta.x * rect.localScale.x / 2 - Screen.width / 2;
-        original.y = transform.position.y - rect.sizeDelta.y * rect.localScale.y / 2 - Screen.height / 2;
-
-        //original.x = rect.anchorMin.x == 0.5f ? rect.position.x : rect.position.x + rect.sizeDelta.x / 2;
-        //original.y = rect.anchorMin.y == 0.5f ? rect.position.y : rect.position.y - rect.sizeDelta.y / 2;
+        posStart.x = eventData.position.x - Screen.width / 2;
+        posStart.y = eventData.position.y - Screen.height / 2;
+        line.Points[0] = posStart;
         line.enabled = true;
-        line.Points[0] = original;
+        attack = false;
+        parent = transform.parent;
+        cardAttack = transform;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Vector2 pos;
-        pos.x = eventData.position.x - Screen.width / 2;
-        pos.y = eventData.position.y - Screen.height / 2;
-        line.Points[1] = pos;
-        line.Resoloution = (pos - original).magnitude / 100;
-        arrow.up = pos - original;
-        arrow.GetComponent<RectTransform>().anchoredPosition = pos;
+        posEnd.x = eventData.position.x - Screen.width / 2;
+        posEnd.y = eventData.position.y - Screen.height / 2;
+        line.Points[1] = posEnd;
+
+        line.Resoloution = (posEnd - posStart).magnitude / 100;
+        
+        arrow.up = posEnd - posStart;
+        arrow.GetComponent<RectTransform>().anchoredPosition = posEnd;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         line.enabled = false;
         arrow.position = Vector2.one * 3000;
+        attack = true;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (attack && transform.parent != parent)
+        {
+            attack = false;
+            cardTarget = transform;
+            StartCoroutine(Attack());
+        }
+    }
+
+    private IEnumerator Attack()
+    {
+        parent.SetAsLastSibling();
+
+        Vector3 pos = cardAttack.position;
+
+        while (cardAttack.position.y != cardTarget.position.y)
+        {
+            cardAttack.position = Vector3.Lerp(cardAttack.position, cardTarget.position, 0.8f * Time.deltaTime * 10);
+            yield return null;
+        }
+
+        cardTarget = null;
+        cardAttack.position = pos;
     }
 }
